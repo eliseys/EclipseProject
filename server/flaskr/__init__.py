2151d397995aae0b82cd3aadf3ab1c88f0acdb73
eliseys <kolesnikovkda@gmail.com>
2020-09-14 09:41:28 +0300

    sent json file to user

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -3,9 +3,8 @@ import time
 import numpy as np
 from skyfield.api import load
 from skyfield.api import Topos
-import eclipseTimes
+from . import eclipseTimes
 from flask import Flask, render_template, request, jsonify, after_this_request
-
 
 
 planets = load('de421.bsp')
@@ -21,7 +20,9 @@ mid = eclipseTimes.t_center(date, p, 1e-4).utc
 c3 = eclipseTimes.t_c3(date, p, r_moon, r_sun, 1e-5).utc
 c4 = eclipseTimes.t_c4(date, p, r_moon, r_sun, 1e-5).utc
 
-eclipseCircumstances = {'C1':c1, 'C2':c2, 'MID':mid, 'C3':c3, 'C4':c4}
+#print("***********", type(c1.year), c1.year)
+
+# eclipseCircumstances = {'C1':c1[0], 'C2':c2[0], 'MID':mid[0], 'C3':c3[0], 'C4':c4[0]}
 
 def create_app(test_config=None):
     # create and configure the app
@@ -44,6 +45,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+
     
     @app.route('/request', methods=['GET'])
     def request():
@@ -53,8 +55,20 @@ def create_app(test_config=None):
             return response
 
         #jsonResp = {'time1': time.strftime("%c"), 'time2': time.strftime("%c")}
+        #jsonResp = {'time1': c1, 'time2': time.strftime("%c")}
+
         #print(jsonResp)
-        return jsonify(eclipseCircumstances)
+
+        #eclipseCircumstances = {'C1':c1, 'C2':c2, 'MID':mid, 'C3':c3, 'C4':c4}
+
+        
+        EC = {'C1': {'year': float(c1.year), 'month': float(c1.month), 'day': float(c1.day), 'hour': float(c1.hour), 'minute': float(c1.minute), 'second': float(c1.second)},
+              'C2': {'year': float(c2.year), 'month': float(c2.month), 'day': float(c2.day), 'hour': float(c2.hour), 'minute': float(c2.minute), 'second': float(c2.second)},
+              'mid': {'year': float(mid.year), 'month': float(mid.month), 'day': float(mid.day), 'hour': float(mid.hour), 'minute': float(mid.minute), 'second': float(mid.second)},
+              'C3': {'year': float(c3.year), 'month': float(c3.month), 'day': float(c3.day), 'hour': float(c3.hour), 'minute': float(c3.minute), 'second': float(c3.second)},
+              'C4': {'year': float(c4.year), 'month': float(c4.month), 'day': float(c4.day), 'hour': float(c4.hour), 'minute': float(c4.minute), 'second': float(c4.second)},
+        }
+        return jsonify(EC)
 
     
     @app.route('/time')
--- server/flaskr/static/app.js
+++ server/flaskr/static/app.js
@@ -7,4 +7,30 @@ let url = 'http://localhost:5000/request';
 // }, 1000);
 
 
-fetch(url).then(response => response.json()).then((json) => {console.log(json)})
+//fetch(url).then(response => response.json()).then((json) => {console.log(json)})
+
+// const intervalID = window.setInterval(function(){
+//     fetch(url).then(response => response.json()).then((json) => {document.getElementById('sample').innerHTML = [json['C1']['year'], json['C1']['month'], ]})
+// }, 1000);
+
+
+// fetch(url).then(response => response.json()).then((json) => {const CE = json})
+// console.log(CE)
+
+
+
+async function fetch_CE() {
+    const response = await fetch(url);
+    
+    if (response.ok) {
+	// если HTTP-статус в диапазоне 200-299
+	// получаем тело ответа (см. про этот метод ниже)
+	var CE = await response.json();
+    } else {
+	alert("HTTP error: " + response.status);
+    }
+
+    return CE
+}
+
+console.log(fetch_CE())
