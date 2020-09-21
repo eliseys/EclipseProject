1617da656fc673db79a38d6baa0affd8a7d7d201
Dmitry Kolesnikov <dmitrykolesnikov@localhost.localdomain>
2020-09-21 19:10:01 +0300

    interface improvements

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -50,27 +50,98 @@ def create_app(test_config=None):
         with open('/home/dmitrykolesnikov/CODE/EclipseProject/skyfield/ec.json', "r") as jf:
             ec = ujson.load(jf)
 
-        now = datetime.now()
+        now = datetime.utcnow() #+ timedelta(days=84, hours=23, minutes=33)
 
-        ec['now'] = {'year': int(now.year), 'month': int(now.month), 'day': int(now.day), 'hour': int(now.hour), 'minute': int(now.minute), 'second': float(now.second)}
+        ec['now'] = {'year': int(now.year), 'month': int(now.month), 'day': int(now.day), 'hour': int(now.hour), 'minute': int(now.minute), 'second': float(now.second + now.microsecond*1e-6)}
+
+        #print('********', ec['now'])
         
         c1 = datetime(ec['c1']['year'], ec['c1']['month'], ec['c1']['day'], ec['c1']['hour'], ec['c1']['minute'], int(math.floor(ec['c1']['second'])), int((ec['c1']['second']%1)*1e6))
         c2 = datetime(ec['c2']['year'], ec['c2']['month'], ec['c2']['day'], ec['c2']['hour'], ec['c2']['minute'], int(math.floor(ec['c2']['second'])), int((ec['c2']['second']%1)*1e6))
         mid = datetime(ec['mid']['year'], ec['mid']['month'], ec['mid']['day'], ec['mid']['hour'], ec['mid']['minute'], int(math.floor(ec['mid']['second'])), int((ec['mid']['second']%1)*1e6))
         c3 = datetime(ec['c3']['year'], ec['c3']['month'], ec['c3']['day'], ec['c3']['hour'], ec['c3']['minute'], int(math.floor(ec['c3']['second'])), int((ec['c3']['second']%1)*1e6))
         c4 = datetime(ec['c4']['year'], ec['c4']['month'], ec['c4']['day'], ec['c4']['hour'], ec['c4']['minute'], int(math.floor(ec['c4']['second'])), int((ec['c4']['second']%1)*1e6))
-        
-        ec['countdown_c1'] = {'days': (c1-now).days,
-                              'hours': (c1-now).seconds//3600,
-                              'minutes': ((c1-now).seconds % 3600) // 60,
-                              'seconds': ((c1-now).seconds % 60) + ((c1-now).microseconds/1e6)
-        }
-        # ec['countdown_c2'] = now-c2
-        # ec['countdown_mid'] = now-mid
-        # ec['countdown_c3'] = now-c3
-        # ec['countdown_c4'] = now-c4
 
-        print("******",  ec['countdown_c1'])
+
+        if c1 >= now:
+            ec['countdown_c1'] = {'sign': '+',
+                                  'days': (c1-now).days,
+                                  'hours': (c1-now).seconds//3600,
+                                  'minutes': ((c1-now).seconds % 3600) // 60,
+                                  'seconds': ((c1-now).seconds % 60) + ((c1-now).microseconds/1e6)
+            }
+        elif c1 < now:
+            ec['countdown_c1'] = {'sign': '-',
+                                  'days': (now-c1).days,
+                                  'hours': (now-c1).seconds//3600,
+                                  'minutes': ((now-c1).seconds % 3600) // 60,
+                                  'seconds': ((now-c1).seconds % 60) + ((now-c1).microseconds/1e6)
+            }
+
+
+            
+        if c2 >= now:
+            ec['countdown_c2'] = {'sign': '+',
+                                  'days': (c2-now).days,
+                                  'hours': (c2-now).seconds//3600,
+                                  'minutes': ((c2-now).seconds % 3600) // 60,
+                                  'seconds': ((c2-now).seconds % 60) + ((c2-now).microseconds/1e6)
+            }
+        elif c2 < now:
+            ec['countdown_c2'] = {'sign': '-',
+                                  'days': (now-c2).days,
+                                  'hours': (now-c2).seconds//3600,
+                                  'minutes': ((now-c2).seconds % 3600) // 60,
+                                  'seconds': ((now-c2).seconds % 60) + ((now-c2).microseconds/1e6)
+            }
+
+        if mid >= now:
+            ec['countdown_mid'] = {'sign': '+',
+                                  'days': (mid-now).days,
+                                  'hours': (mid-now).seconds//3600,
+                                  'minutes': ((mid-now).seconds % 3600) // 60,
+                                  'seconds': ((mid-now).seconds % 60) + ((mid-now).microseconds/1e6)
+            }
+        elif mid < now:
+            ec['countdown_mid'] = {'sign': '-',
+                                  'days': (now-mid).days,
+                                  'hours': (now-mid).seconds//3600,
+                                  'minutes': ((now-mid).seconds % 3600) // 60,
+                                  'seconds': ((now-mid).seconds % 60) + ((now-mid).microseconds/1e6)
+            }
+            
+        if c3 >= now:
+            ec['countdown_c3'] = {'sign': '+',
+                                  'days': (c3-now).days,
+                                  'hours': (c3-now).seconds//3600,
+                                  'minutes': ((c3-now).seconds % 3600) // 60,
+                                  'seconds': ((c3-now).seconds % 60) + ((c3-now).microseconds/1e6)
+            }
+            
+        elif c3 < now:
+            ec['countdown_c3'] = {'sign': '-',
+                                  'days': (now-c3).days,
+                                  'hours': (now-c3).seconds//3600,
+                                  'minutes': ((now-c3).seconds % 3600) // 60,
+                                  'seconds': ((now-c3).seconds % 60) + ((now-c3).microseconds/1e6)
+            }
+
+            
+        if c4 >= now:
+            ec['countdown_c4'] = {'sign': '+',
+                                  'days': (c4-now).days,
+                                  'hours': (c4-now).seconds//3600,
+                                  'minutes': ((c4-now).seconds % 3600) // 60,
+                                  'seconds': ((c4-now).seconds % 60) + ((c4-now).microseconds/1e6)
+            }
+        elif c4 < now:
+            ec['countdown_c4'] = {'sign': '-',
+                                  'days': (now-c4).days,
+                                  'hours': (now-c4).seconds//3600,
+                                  'minutes': ((now-c4).seconds % 3600) // 60,
+                                  'seconds': ((now-c4).seconds % 60) + ((now-c4).microseconds/1e6)
+            }
+
         
         
         return jsonify(ec)
--- server/flaskr/static/app.js
+++ server/flaskr/static/app.js
@@ -13,7 +13,6 @@ let url = 'http://localhost:5000/request';
 //fetch(url).then(response => response.json()).then((json) => {document.getElementById('sample').innerHTML = json['c1']['year']})
 
 
-
 const intervalID = window.setInterval(function(){
 fetch(url)
     .then(response => response.json())
@@ -23,7 +22,7 @@ fetch(url)
 	      document.getElementById("longitude").innerHTML = `${ec['gps']['longitude']}`
 	      document.getElementById("altitude_m").innerHTML = `${ec['gps']['elevation_m']}`
 	      document.getElementById("date_now").innerHTML = `${ec['now']['year']}/${('0'+ec['now']['month']).slice(-2)}/${('0'+ec['now']['day']).slice(-2)}`
-	      document.getElementById("time_now").innerHTML = `${('0'+ec['now']['hour']).slice(-2)}:${('0'+ec['now']['minute']).slice(-2)}:${('0'+(ec['now']['second']).toFixed(0)).slice(-2)}`
+	      document.getElementById("time_now").innerHTML = `${('0'+ec['now']['hour']).slice(-2)}:${('0'+ec['now']['minute']).slice(-2)}:${('0'+(ec['now']['second']).toFixed(1)).slice(-4)}`
 	      document.getElementById("date_c1").innerHTML = `${ec['c1']['year']}/${('0'+ec['c1']['month']).slice(-2)}/${('0'+ec['c1']['day']).slice(-2)}`
 	      document.getElementById("time_c1").innerHTML = `${('0'+ec['c1']['hour']).slice(-2)}:${('0'+ec['c1']['minute']).slice(-2)}:${('0'+(ec['c1']['second']).toFixed(1)).slice(-4)}`
 	      document.getElementById("date_c2").innerHTML = `${ec['c2']['year']}/${('0'+ec['c2']['month']).slice(-2)}/${('0'+ec['c2']['day']).slice(-2)}`
@@ -34,11 +33,11 @@ fetch(url)
 	      document.getElementById("time_c3").innerHTML = `${('0'+ec['c3']['hour']).slice(-2)}:${('0'+ec['c3']['minute']).slice(-2)}:${('0'+(ec['c3']['second']).toFixed(1)).slice(-4)}`
 	      document.getElementById("date_c4").innerHTML = `${ec['c4']['year']}/${('0'+ec['c4']['month']).slice(-2)}/${('0'+ec['c4']['day']).slice(-2)}`
 	      document.getElementById("time_c4").innerHTML = `${('0'+ec['c4']['hour']).slice(-2)}:${('0'+ec['c4']['minute']).slice(-2)}:${('0'+(ec['c4']['second']).toFixed(1)).slice(-4)}`
-	      document.getElementById("countdown_c1").innerHTML = `${ec['countdown_c1'].days}d ${ec['countdown_c1'].hours}h ${ec['countdown_c1'].minutes}m ${ec['countdown_c1'].seconds.toFixed(1)}s`
-	      document.getElementById("countdown_c2").innerHTML = `${ec['countdown_c2'].days}d ${ec['countdown_c2'].hours}h ${ec['countdown_c2'].minutes}m ${ec['countdown_c2'].seconds.toFixed(1)}s`
-	      document.getElementById("countdown_mid").innerHTML = `${ec['countdown_mid'].days}d ${ec['countdown_mid'].hours}h ${ec['countdown_mid'].minutes}m ${ec['countdown_mid'].seconds.toFixed(1)}s`
-	      document.getElementById("countdown_c3").innerHTML = `${ec['countdown_c3'].days}d ${ec['countdown_c3'].hours}h ${ec['countdown_c3'].minutes}m ${ec['countdown_c3'].seconds.toFixed(1)}s`
-	      document.getElementById("countdown_c4").innerHTML = `${ec['countdown_c4'].days}d ${ec['countdown_c4'].hours}h ${ec['countdown_c4'].minutes}m ${ec['countdown_c4'].seconds.toFixed(1)}s`
+	      document.getElementById("countdown_c1").innerHTML = `${ec.countdown_c1.sign}${ec['countdown_c1'].days}d ${('0'+ec['countdown_c1'].hours).slice(-2)}h ${('0'+ec['countdown_c1'].minutes).slice(-2)}m ${('0'+ec['countdown_c1'].seconds.toFixed(1)).slice(-4)}s`
+	      document.getElementById("countdown_c2").innerHTML = `${ec.countdown_c2.sign}${ec['countdown_c2'].days}d ${('0'+ec['countdown_c2'].hours).slice(-2)}h ${('0'+ec['countdown_c2'].minutes).slice(-2)}m ${('0'+ec['countdown_c2'].seconds.toFixed(1)).slice(-4)}s`
+	      document.getElementById("countdown_mid").innerHTML = `${ec.countdown_mid.sign}${ec['countdown_mid'].days}d ${('0'+ec['countdown_mid'].hours).slice(-2)}h ${('0'+ec['countdown_mid'].minutes).slice(-2)}m ${('0'+ec['countdown_mid'].seconds.toFixed(1)).slice(-4)}s`
+	      document.getElementById("countdown_c3").innerHTML = `${ec.countdown_c3.sign}${ec.countdown_c3.days}d ${('0'+ec['countdown_c3'].hours).slice(-2)}h ${('0'+ec['countdown_c3'].minutes).slice(-2)}m ${('0'+ec['countdown_c3'].seconds.toFixed(1)).slice(-4)}s`
+	      document.getElementById("countdown_c4").innerHTML = `${ec.countdown_c4.sign}${ec.countdown_c4.days}d ${('0'+ec['countdown_c4'].hours).slice(-2)}h ${('0'+ec['countdown_c4'].minutes).slice(-2)}m ${('0'+ec.countdown_c4.seconds.toFixed(1)).slice(-4)}s`
 	  }
 	 )
 }, 100);
