59afabf970e38b4eb30bcdcf9faa258840fa2593
eliseys <kolesnikovkda@gmail.com>
2020-11-24 00:15:31 +0300

    camera comtrol added main settings

--- camera_control/imageCapture.py
+++ camera_control/imageCapture.py
@@ -8,54 +8,89 @@ import schedule
 
 
 
-def job_that_executes_once():
+# def job_that_executes_once():
 
-    print("TEST SCHEDULE", datetime.now())
+#     print("TEST SCHEDULE", datetime.now())
     
-    return schedule.CancelJob
+#     return schedule.CancelJob
 
 
 
-#job_that_executes_once()
+# #job_that_executes_once()
 
-schedule.every().day.at('17:36:40').do(job_that_executes_once)
+# schedule.every().day.at('17:36:40').do(job_that_executes_once)
 
-while True:
-    schedule.run_pending()
-
-
+# while True:
+#     schedule.run_pending()
 
 
 
-
-#sleep(60)
-
-
-detect_cameras = ["--auto-detect"]
-
-lockup_mirror = ["--set-config", "viewfinder" + "=1"]
-release_mirror = ["--set-config", "viewfinder" + "=0"]
-
-#lockup_mirror = ["--capture-preview"]
+# if camera busy
+# kill output of the following
+# ps aux | grep gphoto
 
 
-
-#set_parameters = [""]
-
-time = "=1.0s" #
-frames = "=2f"
-
-wait_time = ["--wait-event" + time]
-wait_frames = ["--wait-event" + frames]
-
-wait_0 = ["--wait-event" + "=100ms"]
+# gp("--auto-detect")
 
 
-capture = ["--trigger-capture"]
+# settings
+
+gp("--set-config", "capturetarget=1") # Capture Target Memory card
+print(gp("--get-config", "capturetarget"))
+
+# imgsettings
+
+gp("--set-config", "imageformat=RAW") # Image Format 
+print(gp("--get-config", "imageformat"))
+gp("--set-config", "imageformatsd=RAW") # Image Format SD
+print(gp("--get-config", "imageformat"))
+gp("--set-config", "imageformatcf=RAW") # Image Format CF
+print(gp("--get-config", "imageformat"))
+
+gp("--set-config", "iso=100") # ISO
+print(gp("--get-config", "iso"))
+
+gp("--set-config", "whitebalance=Daylight") # WhiteBalance
+print(gp("--get-config", "whitebalance"))
+
+# capturesettings
+
+gp("--set-config", "exposurecompensation=0") # Exposure Compensation
+print(gp("--get-config", "exposurecompensation"))
+
+gp("--set-config", "highisonr=2") # High ISO Noise Reduction (2=off)
+print(gp("--get-config", "highisonr"))
+
+gp("--set-config", "drivemode=1") # Drive Mode Continuous high speed
+print(gp("--get-config", "drivemode"))
+
+gp("--set-config", "drivemode=2") # Drive Mode Continuous low speed
+print(gp("--get-config", "drivemode"))
+
+gp("--set-config", "picturestyle=Neutral") # Picture Style
+print(gp("--get-config", "picturestyle"))
+
+#gp("--set-config", "aperture") # Aperture
+print(gp("--get-config", "aperture"))
+
+gp("--set-config", "shutterspeed") # Shutter Speed
+print(gp("--get-config", "shutterspeed"))
+
+gp("--set-config", "aeb=0") # Auto Exposure Bracketing
+print(gp("--get-config", "aeb"))
+
+
+
+
+#######################################
+
+
+
+
+
 
 
 #burst_shooting = ["--set-config", "eosremoterelease" + "=2", "--wait-event" + time, "--set-config", "eosremoterelease" + "=4"]
-
 
 #burst_shooting_start = ["--set-config", "eosremoterelease" + "=5", "--wait-event" + "=5"] # Immediate
 
@@ -74,99 +109,6 @@ liveview_off = ["--set-config", "output" + "=0"]
 remoterelease = ["--set-config", "eosremoterelease" + "=5"]
 remoterelease_press_full = ["--set-config", "eosremoterelease" + "=2"]
 
-#bulb?
-
-
-iso = "=100"
-
-set_iso = ["--set-config", "iso" + iso]
-
-
-shutterspeed = "=1/500"
-set_shutterspeed = ["--set-config", "shutterspeed" + shutterspeed]
-
-
-drivemode_single = ["--set-config", "drivemode" + "=0"] 
-drivemode_high_speed = ["--set-config", "drivemode" + "=1"] 
-
-#print(gp(detect_cameras))#
-
-
-
-
-#gp(capture)#
-#gp(lockup_mirror)
-
-
-#gp(capture)
-#sleep(2)
-
-#gp(set_shutterspeed)
-#gp(burst_shootingx)
-#gp(set_iso)
-
-#gp(lockup_mirror)
-#sleep(1)
-#gp(release_mirror)
-
-#gp(liveview, drivemode_high_speed, wait_time, remoterelease, wait_time, burst_shooting_stop)
-#sleep(1.0)
-#gp(drivemode_single, liveview, wait_time, "--wait-event=1s", gp(remoterelease_press_full), "--wait-event=1s", gp(remoterelease_press_full))
-#gp(drivemode_single, liveview, "--wait-event=2s", capture, "--wait-event=FILEADDED", capture, )
-
-
-
-
-# /main/actions/syncdatetime
-
-
-# /main/actions/eosremoterelease
-# # Label: Canon EOS Remote Release
-# # Readonly: 0
-# # Type: RADIO
-# # Current: None
-# # Choice: 0 None
-# # Choice: 1 Press Half
-# # Choice: 2 Press Full
-# # Choice: 3 Release Half
-# # Choice: 4 Release Full
-# # Choice: 5 Immediate
-# # Choice: 6 Press 1
-# # Choice: 7 Press 2
-# # Choice: 8 Press 3
-# # Choice: 9 Release 1
-# # Choice: 10 Release 2
-# # Choice: 11 Release 3
-
-
-
-# /main/settings/output
-
-# Choice: 0 Off
-# Choice: 1 TFT
-
-
-
-
-# /main/imgsettings/iso
-# Label: ISO Speed
-
-
-# autoexposuremode
-
-# 3 Manual
-
-
-
-
-# drivemode
-
-# aperture
-
-# shutterspeed
-
-
-#t = 0.25
 
 
 #gp("--set-config", "output=1", "--wait-event", "1.0s", "--capture-image", "-I 10s", "-F 5", "--set-config", "output=0")
@@ -189,29 +131,3 @@ drivemode_high_speed = ["--set-config", "drivemode" + "=1"]
 #11.020 total
 
 
-# gp("--capture-image")
-# sleep(t)
-# gp("--capture-image")
-# sleep(t)
-# gp("--capture-image")
-# sleep(t)
-# gp("--capture-image")
-
-
-#sleep(1.000)
-#gp(burst_shooting_stop, liveview)
-
-
-# gp(lockup_mirror, burst_shooting_start)
-# sleep(0.500)
-# gp(lockup_mirror, burst_shooting_stop)
-#sleep(1)
-#gp(release_mirror)
-
-
-# k = 0
-# while k<5:
-
-#     gp(capture)
-#     k = k + 1
-#     sleep(0.1)
