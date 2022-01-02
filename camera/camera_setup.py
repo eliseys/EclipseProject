5882b771d51b32bbf62a2929c39ac62d6302fad4
eliseys <kolesnikovkda@gmail.com>
2022-01-02 23:31:18 +0300

    sync

--- camera/camera_setup.py
+++ camera/camera_setup.py
@@ -6,18 +6,21 @@ from sh import gphoto2 as gp
 
 print(gp("--auto-detect"))
 
-print("*******************")
-print("Canon EOS 7D MarkII")
-print("*******************")
+#print("*******************")
+#print("Canon EOS 7D MarkII")
+#print("*******************")
 
+# Capture Target Memory card
+# save files to internal memory card
 
-gp("--camera", "Canon EOS 7D MarkII", "--set-config", "capturetarget=1") # Capture Target Memory card
+gp("--camera", "Canon EOS 7D MarkII", "--set-config", "capturetarget=1") 
 print(gp("--get-config", "capturetarget"))
 
-# imgsettings
+# image settings
 
 gp("--camera", "Canon EOS 7D MarkII", "--set-config", "imageformat=RAW") # Image Format 
 print(gp("--camera", "Canon EOS 7D MarkII", "--get-config", "imageformat"))
+
 gp("--camera", "Canon EOS 7D MarkII", "--set-config", "imageformatcf=RAW") # Image Format CF
 print(gp("--camera", "Canon EOS 7D MarkII", "--get-config", "imageformatcf"))
 
@@ -50,43 +53,4 @@ print(gp("--camera", "Canon EOS 7D MarkII", "--get-config", "shutterspeed"))
 gp("--camera", "Canon EOS 7D MarkII", "--set-config", "aeb=2") # Auto Exposure Bracketing
 print(gp("--camera", "Canon EOS 7D MarkII", "--get-config", "aeb"))
 
-print("*******************")
-print("Canon EOS RP")
-print("*******************")
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "capturetarget=1") # Capture Target Memory card
-print(gp("--get-config", "capturetarget"))
-
-# imgsettings
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "imageformat=RAW") # Image Format 
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "imageformat"))
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "iso=100") # ISO
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "iso"))
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "whitebalance=Daylight") # WhiteBalance
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "whitebalance"))
-
-# capturesettings
-
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "exposurecompensation"))
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "highisonr=2") # High ISO Noise Reduction (2=off)
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "highisonr"))
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "drivemode=1") # Drive Mode Continuous high speed
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "drivemode"))
-
-#gp("--camera", "USB PTP Class Camera", "--set-config", "drivemode=2") # Drive Mode Continuous low speed
-#print(gp("--camera", "USB PTP Class Camera", "--get-config", "drivemode"))
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "picturestyle=Neutral") # Picture Style
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "picturestyle"))
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "shutterspeed=0.5") # Shutter Speed
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "shutterspeed"))
-
-gp("--camera", "USB PTP Class Camera", "--set-config", "aeb=3") # Auto Exposure Bracketing
-print(gp("--camera", "USB PTP Class Camera", "--get-config", "aeb"))
 
